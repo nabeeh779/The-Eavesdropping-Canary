d3f3561932458a9cd0265e76c046f15d8186e7ce
nabeeh997 <nbeh33@hotmail.com>
2025-05-18 15:07:04 +0300

    make use of logger during run

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
 from scapy.all import sniff, conf, Packet, PacketList,NetworkInterface, get_if_list, IP, TCP, Raw
 from netaddr import IPNetwork, IPAddress
 from typing import Optional, List
-
+from logger import CanaryLogger
 DEAFULT_INTERFACE: int = -1
 LOOPBACK_ADDRESS: str = "0.0.0.0"
 LOCAL_IP_ADDRESS_INDEX: int = 4
 CANARY_SERVICE_RANGE = range(9000, 9011)
 TCP_FLAGS = ['A' , 'S'] # ACK, SYN
 
-
-class enum:
-    ACK='A'
+my_logger = CanaryLogger()
 
 def is_local_ip(ip) -> bool:
     ''' This function checks if given ip belongs to local network '''
@@ -33,10 +31,11 @@ def handle_sus_connection(packet: Packet):
     '''Handle suspicious connection'''
     print(f'Suspicious connection detected form [src_Ip = {packet[IP].src}:{packet[IP].sport}] to [dst_ip = {packet[IP].dst}:{packet[TCP].dport}]')
     print(f"Protocol: TCP/{packet[TCP].dport}")
+    data = ''
     if packet.haslayer(Raw):
         data = packet[Raw].load.decode(errors="ignore")
         print(f'payload:\n{data}')
-
+    my_logger.log_alert(packet[IP].src, packet[IP].sport, packet[IP].dst, packet[TCP].dport, data)
 
 def proccess_capture_packets(packet: Packet):
     '''
@@ -47,7 +46,6 @@ def proccess_capture_packets(packet: Packet):
         if is_local_ip(packet[IP].src) and (packet[TCP].dport in CANARY_SERVICE_RANGE):
             if (packet[TCP].flags == TCP_FLAGS[0] or packet[TCP].flags == TCP_FLAGS[1]):
                 print('Captured TCP connection handshake ')
-
             handle_sus_connection(packet)
 
 
@@ -89,6 +87,8 @@ def get_desierd_interface() -> Optional[str]:
         print("Invalid choice")
 
     return None
+
+
 def main():
     interface: str = get_desierd_interface()
     #print(interface)
