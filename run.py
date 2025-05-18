804cb8fd30d152746c0b8e0d819b27da5e966fe6
nabeeh997 <nbeh33@hotmail.com>
2025-05-18 14:27:18 +0300

    capture tcp packets spesfic for canay ports

--- run.py
+++ run.py
@@ -1,21 +1,57 @@
-from scapy.all import sniff, conf, Packet, NetworkInterface, get_if_list
+from scapy.all import sniff, conf, Packet, PacketList,NetworkInterface, get_if_list, IP, TCP, Raw
+from netaddr import IPNetwork, IPAddress
 from typing import Optional, List
 
 DEAFULT_INTERFACE: int = -1
+LOOPBACK_ADDRESS: str = "0.0.0.0"
+LOCAL_IP_ADDRESS_INDEX: int = 4
+CANARY_SERVICE_RANGE = range(9000, 9011)
+
+def is_local_ip(ip) -> bool:
+    ''' This function checks if given ip belongs to local network '''
+    local_networks = []
+
+    for add in conf.route.routes:
+        #if add != LOOPBACK_ADDRESS:
+            # add network subnet to list
+        local_networks.append(add[LOCAL_IP_ADDRESS_INDEX])
+
+    for network in local_networks:
+        #import ipdb; ipdb.set_trace()
+        if IPAddress(ip) in IPNetwork(network):
+            return True
+
+    return False
+
+def handle_sus_connection(packet: Packet):
+    '''Handle suspicious connection'''
+    print(f'Suspicious connection detected form [Ip = {packet[IP].src}, dst_port= {packet[TCP].dport}]')
+    data = packet[Raw].load.decode(errors="ignore")
+    if data:
+        print(f'payload:\n{data}')
 def proccess_capture_packets(packet: Packet):
-    '''This function handle captuerd packets'''
-    print(packet.summary())
+    '''
+        This function handle captuerd packets, and looks for packets includes
+        IP belongs to our local network and have tco layer
+    '''
+    if packet.haslayer(IP) and packet.haslayer(TCP):
+        if is_local_ip(packet[IP].src) and (packet[TCP].dport in CANARY_SERVICE_RANGE):
+            handle_sus_connection(packet)
+
+
 
 def start_sniffer(interface_name: str  = conf.iface):
     '''This function start sniffing by given interface'''
     print(f"Sniffing on {interface_name}")
     try:
-        packets = sniff(iface=interface_name,prn=proccess_capture_packets)
+        packets: PacketList = sniff(iface=interface_name,prn=proccess_capture_packets)
     except PermissionError:
         # in case we need sudo or run by admin
         print("Need admin privileges")
     except Exception as e:
         print(f"Sniffing error: {e}")
+
+
 
 def get_desierd_interface() -> Optional[str]:
     '''This function prints all available interfaces and return user choice'''
